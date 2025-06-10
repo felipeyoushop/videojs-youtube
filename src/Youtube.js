d668566fa4064497e03f23405aed51ed0e98b4ef
Felipe Augusto Sviaghin Ferri <felipe.ferri@gmail.com>
2025-06-10 11:13:17 -0300

    Adds shorts support

--- src/Youtube.js
+++ src/Youtube.js
@@ -709,7 +709,7 @@ THE SOFTWARE. */
       videoId: null
     };
 
-    var regex = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var regex = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|shorts\/|watch\?v=|\&v=)([^#\&\?]*).*/;
     var match = url.match(regex);
 
     if (match && match[2].length === 11) {
